6d588d85f6dffac783ce12ab6f45de8096d73fe8
piacentine <98316285+piacentine@users.noreply.github.com>
2022-01-24 11:33:16 +0100

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -11,14 +11,14 @@ declare namespace IR_V15 {
      */
     //% blockId=ir_received_left_event
     //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: () => void): void;
+    export function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
     //% block="connect ir receiver to %pin" shim=Mbit_IR::init
-    function init(pin: Pins): void;
+    export function init(pin: Pins): void;
 }
 
 // Auto-generated. Do not edit. Really.
